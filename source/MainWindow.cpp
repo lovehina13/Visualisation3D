f0a33f8f345c7ba7a21cf4d6756bf679e5a02c36
Alexis Foerster <alexis.foerster@gmail.com>
2017-07-05 23:27:10 +0200

    Implémentation de la classe MainWindow (non terminée).

--- source/MainWindow.cpp
+++ source/MainWindow.cpp
@@ -12,6 +12,20 @@ MainWindow::MainWindow(QWidget* parent) :
         QMainWindow(parent), ui(new Ui::MainWindow)
 {
     this->ui->setupUi(this);
+
+    connect(this->ui->sliderRotationX, SIGNAL(valueChanged(int)), this->ui->spinBoxRotationX,
+            SLOT(setValue(int)));
+    connect(this->ui->sliderRotationY, SIGNAL(valueChanged(int)), this->ui->spinBoxRotationY,
+            SLOT(setValue(int)));
+    connect(this->ui->sliderRotationZ, SIGNAL(valueChanged(int)), this->ui->spinBoxRotationZ,
+            SLOT(setValue(int)));
+
+    connect(this->ui->spinBoxRotationX, SIGNAL(valueChanged(int)), this->ui->sliderRotationX,
+            SLOT(setValue(int)));
+    connect(this->ui->spinBoxRotationY, SIGNAL(valueChanged(int)), this->ui->sliderRotationY,
+            SLOT(setValue(int)));
+    connect(this->ui->spinBoxRotationZ, SIGNAL(valueChanged(int)), this->ui->sliderRotationZ,
+            SLOT(setValue(int)));
 }
 
 MainWindow::~MainWindow()
